2bcd4d69d56f6e4db8a30fe2c206b420e4784600
Christopher Brown <io@henrian.com>
2018-06-18 19:22:18 -0500

    Split main into main/provider per newer instructions

---
+++ provider.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"github.com/hashicorp/terraform/helper/schema"
+	"os/exec"
+)
+
+func resolveIPAddress() (ipAddress string, err error) {
+	stdoutBytes, err := exec.Command("dig", "+short", "myip.opendns.com", "@resolver1.opendns.com").Output()
+	if err != nil {
+		return
+	}
+	ipAddress = string(bytes.TrimSpace(stdoutBytes))
+	return
+}
+
+func resource() *schema.Resource {
+	return &schema.Resource{
+		Read: resourceRead,
+		Schema: map[string]*schema.Schema{
+			"wan_ip_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceRead(d *schema.ResourceData, meta interface{}) error {
+	ipAddress, err := resolveIPAddress()
+	if err != nil {
+		return err
+	}
+	// you must set the Id; otherwise other resources can't access anything set in this method
+	d.SetId("_network_info")
+	d.Set("wan_ip_address", ipAddress)
+	return nil
+}
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		// DataSourcesMap is like ResourcesMap but points to Resources that only have Read and Schema implemented
+		DataSourcesMap: map[string]*schema.Resource{
+			"network_info": resource(),
+		},
+	}
+}
